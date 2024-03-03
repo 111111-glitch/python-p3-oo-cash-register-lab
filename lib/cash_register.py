b9da6f0491de3aeec5582038a0a70456a8764202
111111-glitch <ngigimelissa@gmail.com>
2024-03-03 20:47:55 +0300

    not complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,29 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0.0
+        self.discount = discount
+        self.items = []
+
+    def add_item(self, title, price, quantity=1):
+        for _ in range(quantity):
+            self.items.append(title)
+            self.total += price
+
+    def apply_discount(self):
+        if self.discount:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            print(f"After the discount, the total comes to ${self.total:.2f}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.items:
+            self.total -= self.items.pop()
+
+    def remove_all_items(self):
+        self.total = 0.0
+        self.items = []
+
+    def get_all_items(self):
+        return self.items
